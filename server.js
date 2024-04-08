42256d330d95ff14aa682b5adf732aa4e77e1819
el634dev <al7758066@gmail.com>
2024-04-08 01:06:49 -0400

    node reddit clone project init

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+// -----------------
+// Handlebars Setup
+const exhbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+
+app.engine('handlebars', exhbs.engine({
+    defaultLayout: 'main'
+}));
+
+// -----------------
+// Setup Handlebars to render
+app.set('view engine', 'handlebars');
+
+// -----------------
+// Routes
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// -----------------
+// Port
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`App listening on ${port}`)
+})
